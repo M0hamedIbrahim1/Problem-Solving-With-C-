38548e9de5cdacd26034d1a0cbb3e43f4387ca0a
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-04-17 07:27:55 +0200

    Create Prime Function.cpp

---
+++ Functions/Prime Function.cpp
@@ -0,0 +1,34 @@
+//link of problem : https://codeforces.com/group/MWSDmqGsZm/contest/223205/problem/D
+
+#include<bits/stdc++.h>
+using namespace std;
+bool func(int num){  
+ int newNumber = sqrt(num); 
+ int flag = 0;
+ for(int i = 2;i<= newNumber;i++){
+ 	if(num % i == 0){
+ 		flag = 1;
+ 		break;
+	 }
+ }
+ return flag;
+}
+int main(){
+	int size;
+	cin>>size;  
+	while(size--){  
+		int num;
+		cin>>num;
+		if(num == 1){
+			cout<<"NO"<<endl;
+			continue;
+		}
+		int pr = func(num);
+		if(pr == 1){
+			cout<<"NO"<<endl;
+		} else {
+			cout<<"YES"<<endl;
+		}
+	}
+	
+}
