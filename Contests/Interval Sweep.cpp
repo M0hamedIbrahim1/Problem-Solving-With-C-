458c410848010fd4c48cbea2b83780798503c538
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-04-17 07:33:26 +0200

    Create Interval Sweep.cpp

---
+++ Contests/Interval Sweep.cpp
@@ -0,0 +1,18 @@
+//link of problem : https://codeforces.com/group/MWSDmqGsZm/contest/326175/problem/E
+
+#include<iostream>
+#include<cmath>
+ 
+using namespace std;
+int main(){
+int n1,n2;
+cin>>n1>>n2;
+ 
+if((n1==0 && n2==0) || abs(n1-n2) >=2){
+	cout<<"NO";
+}
+else{
+	cout<<"YES";
+}
+	}
+ 	
