92790b349de736afe8c3299cdee987d8b0e1bafe
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-04-17 07:30:23 +0200

    Create Prime checking.cpp

---
+++ Math - Geometry/Prime checking.cpp
@@ -0,0 +1,24 @@
+//link of problem : https://codeforces.com/group/MWSDmqGsZm/contest/223338/problem/B
+
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+   long long num;
+   cin>>num;
+   bool isPrime = true;
+   
+   if(num == 0 || num == 1){
+   		isPrime = false;
+   }
+   for(int i=2;i<= sqrt(num);i++){ //
+   		if(num % i == 0){
+   			isPrime = false;
+   			break;
+		}
+   }
+   if(isPrime){
+   	cout<<"YES"<<endl;
+   } else {
+   	   	cout<<"NO"<<endl;
+   }
+}
