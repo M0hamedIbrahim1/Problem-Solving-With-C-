60cf748d83dd4b8e8d354c5b9059f9408a5bf7a2
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-04-26 22:57:09 +0200

    Create pup.cpp

---
+++ Loops_Problems/pup.cpp
@@ -0,0 +1,20 @@
+link : https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/V
+
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+int row,n1=-3,n2=-2,n3=-1;
+cin>>row;
+while(row>0){
+	n1= n1+4;
+	n2=n2+4;
+	n3=n3+4;
+cout<<n1<<" "<<n2<<" "<<n3<<" PUM"<<endl;
+	
+	
+	
+	row--;
+}
+}
