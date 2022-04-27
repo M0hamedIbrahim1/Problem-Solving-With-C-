bbfa8640a3a7e0ac7aed4ebe3908f59279191780
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-04-27 22:37:52 +0200

    Create Pyramid.cpp

---
+++ Loops_Problems/Pyramid.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std; 								  
+int main(){
+ 
+ int row;
+ cin>>row;
+for(int j = 1;j<=row;j++){
+	for(int i =0;i<j;i++){
+			cout<<'*';
+ 
+	}
+	cout<<endl;
+}
+ 
+}
