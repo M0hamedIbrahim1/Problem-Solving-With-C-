677d567dfa75286187377fe0c1ebea887953d89d
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-04-27 22:40:33 +0200

    Create Numbers Histogram.cpp

---
+++ Loops_Problems/Numbers Histogram.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std; 								  
+int main(){
+ char c;
+ cin>>c;
+ int row;
+ cin>>row;
+ while(row>0){
+ 	int num;
+ 	cin>>num;
+ 	for(int i=1;i<=num;i++){
+ 	cout<<c;	
+ 		
+	 }
+ 	cout<<endl;
+ 	
+ 	row--;
+ }
+ 
+}
