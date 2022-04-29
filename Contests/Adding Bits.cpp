fae455aa18372b1a8bef712218bf326fc992598f
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-04-29 22:26:07 +0200

    Create Adding Bits.cpp

---
+++ Contests/Adding Bits.cpp
@@ -0,0 +1,12 @@
+//Link : https://codeforces.com/group/MWSDmqGsZm/contest/326175/problem/F
+// author : @Mohamed Ibrahim 
+#include<iostream>
+using namespace std;						  
+int main(){
+int n1,n2,res=0;
+cin>>n1>>n2;
+
+	res = n1^n2;
+	cout<<res;
+	 
+}
