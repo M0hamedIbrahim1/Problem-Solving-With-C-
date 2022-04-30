210d89903128a5399fbf64ad3ca5b3cd5b15083f
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-04-30 20:39:07 +0200

    Create  Winter Sale.cpp

---
+++ Contests/Winter Sale.cpp
@@ -0,0 +1,13 @@
+// link : https://codeforces.com/group/MWSDmqGsZm/contest/326175/problem/A
+// Author : @Mohamed Ibrahim
+
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main(){
+ double n1,n2,p=0;
+ cin>>n1>>n2;
+ p = n2 / (1 - n1/100 );
+cout <<fixed<<setprecision(2)<<p<<endl; 
+}
+ 
