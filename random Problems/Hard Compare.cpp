2c0cb9f6fa6235551723850a15f6d2578fd26199
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-04-30 22:17:42 +0200

    Create Hard Compare.cpp

---
+++ random Problems/Hard Compare.cpp
@@ -0,0 +1,20 @@
+// link : https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/Z
+// Author : @Mohamed Ibrahim
+
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main(){
+	
+long long a,b,c,d;
+cin>>a>>b>>c>>d;
+ 
+if ( b * log(a) > d * log(c) ){
+	cout<<"YES";
+}
+else{
+	cout<<"NO";
+}
+	
+	
+}
