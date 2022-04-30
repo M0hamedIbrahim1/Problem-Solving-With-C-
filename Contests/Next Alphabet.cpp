7cd4979db221a2be04853c8bad9478deebd8b412
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-04-30 20:35:41 +0200

    Create  Next Alphabet.cpp

---
+++ Contests/Next Alphabet.cpp
@@ -0,0 +1,19 @@
+// link : https://codeforces.com/group/MWSDmqGsZm/contest/326175/problem/C
+// Author : @Mohamed Ibrahim
+
+#include<iostream>
+using namespace std;
+int main(){
+char c;
+cin>>c;
+	if(c!='z'){
+		c+=1;
+		cout<<c;	
+	}
+	else{
+		cout<<"a";	
+	}
+ 
+ 
+	}
+ 
