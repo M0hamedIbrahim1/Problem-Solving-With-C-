17f74005fa5be624a3a6d67b46f026aeae483a72
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-05-03 22:34:58 +0200

    Create Sorting.cpp

---
+++ Arrays/Sorting.cpp
@@ -0,0 +1,34 @@
+//link   :  https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/H
+//author : @Mohamed Ibrahim
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int size; // 5
+	cin>>size;
+	int arr[size];
+	for(int i=0;i<size;i++){ 
+		cin>>arr[i];
+	}
+	for(int i=0;i<size-1;i++){
+		int index;
+		int flag = 0;
+		int myValue = arr[i];
+		for(int z=i+1;z<size;z++){
+			if(arr[z] < myValue){
+				index = z;
+				myValue = arr[z];
+				flag = 1;
+			}
+		}
+		if(flag == 1){
+			int temp = arr[i];
+			arr[i] = myValue;
+			arr[index] = temp;
+		}
+	}
+	for(int i=0;i<size;i++){
+		cout<<arr[i]<<" ";
+	}
+}
