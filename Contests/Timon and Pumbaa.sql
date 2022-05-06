e8c14a43ed42e82b251757f92765bf389308e9a8
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-05-06 21:09:22 +0200

    Create Timon and Pumbaa.sql

---
+++ Contests/Timon and Pumbaa.sql
@@ -0,0 +1,13 @@
+//  link : https://codeforces.com/group/MWSDmqGsZm/contest/326907/problem/A
+//@ Author : Mohamed Ibrahim
+#include<bits/stdc++.h>
+using namespace std;						  
+int main() {
+  long long n1,n2;
+  cin>>n1>>n2;
+  if(n1-n2>=0){
+    cout<<n1-n2;
+  }else{
+    cout<<'0';
+  }
+}
