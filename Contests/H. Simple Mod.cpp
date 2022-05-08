1865cfc1c549e6b0d61c51f265ae8ba7f7ddf527
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-05-08 22:24:42 +0200

    Create H. Simple Mod.cpp

---
+++ Contests/H. Simple Mod.cpp
@@ -0,0 +1,11 @@
+//link : https://codeforces.com/group/MWSDmqGsZm/contest/326907/problem/H
+//author : @Mohamed Ibrahim
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int num;
+	cin>>num;
+	cout<<"0 0"<<endl;
+}
