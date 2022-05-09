b630c41ff7646ce511898b1ca129b48be5a8133a
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-05-09 21:25:17 +0200

    Create A. Create A New String.cpp

---
+++ Strings/A. Create A New String.cpp
@@ -0,0 +1,17 @@
+//link : https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/B
+//author : @Mohamed Ibrahim
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+string s1,s2;
+cin>>s1>>s2;
+
+
+cout<<s1.length()<<" "<<s2.length()<<endl;
+cout<<s1<<" "<<s2;
+
+
+
+}
