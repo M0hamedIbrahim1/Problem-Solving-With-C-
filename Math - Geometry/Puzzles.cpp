067e8538711f0df07e79a3e6c92bfa71c340bc26
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-05-16 23:08:35 +0200

    Create Puzzles.cpp

---
+++ Math - Geometry/Puzzles.cpp
@@ -0,0 +1,27 @@
+//link :https://vjudge.net/contest/492820#problem/P
+//author :@Mohamed Ibrahim
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+ int n,m,res=1500;
+ cin>>n>>m;
+ int arr[m];
+ for(int i=0;i<m;i++){
+ 	cin>>arr[i];
+ }
+ sort(arr,arr+m);
+ --n;
+ 
+  for(int i=0;i<m-n;i++){
+  	if(arr[i+n]-arr[i]<res){
+  		res = arr[i+n]-arr[i];
+	  }
+	
+  }
+cout<<res;
+ 
+return 0;
+}
