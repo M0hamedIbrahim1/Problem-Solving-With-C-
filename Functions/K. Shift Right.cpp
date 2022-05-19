aac5faf6c08a95e553aafe9a7b51c269e1f614c8
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-05-19 19:57:17 +0200

    Create K. Shift Right.cpp

---
+++ Functions/K. Shift Right.cpp
@@ -0,0 +1,29 @@
+//link : https://codeforces.com/group/MWSDmqGsZm/contest/223205/problem/K
+//author  : @Mohamed Ibrahim
+
+#include<bits/stdc++.h>
+using namespace std;
+const int s = 100001;
+void print1(int arr[s],int theShift,int size){
+	for(int i = theShift;i<size;i++){
+		cout<<arr[i]<<" ";
+	}
+}
+void print2(int arr[s],int theShift){
+	for(int i = 0;i<theShift;i++){ 
+		cout<<arr[i]<<" ";
+	}
+}
+int main()
+{
+	int size,times;
+	cin>>size>>times;
+	int arr[s];
+	for(int i=0;i<size;i++){
+		cin>>arr[i];
+	}
+	int sh = times % size;
+	print1(arr,size - sh,size);
+	print2(arr,size - sh);
+	
+}
