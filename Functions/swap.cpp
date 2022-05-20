4d7fb2acfb4a2d192ac97556cae299e93e24bdcb
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-05-20 14:15:50 +0200

    Create swap.cpp

---
+++ Functions/swap.cpp
@@ -0,0 +1,17 @@
+//link :https://codeforces.com/group/MWSDmqGsZm/contest/223205/problem/E
+//author : @Mohamed Ibrahim
+
+#include<bits/stdc++.h>
+using namespace std;
+void swap(int *num1,int *num2){
+	int *box = num1;
+	num1 = num2;
+	num2 = box;
+}
+int main(){
+	int num1,num2;
+	cin>>num1>>num2;
+	swap(num1,num2);
+	cout<<num1<<" "<<num2;
+}
+ 
