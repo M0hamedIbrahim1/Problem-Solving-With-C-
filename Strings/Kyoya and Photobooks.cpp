dbff5446d8f67d9b0d55c2a3b4c29b10b7704700
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-05-27 14:39:36 +0200

    Create Kyoya and Photobooks.cpp

---
+++ Strings/Kyoya and Photobooks.cpp
@@ -0,0 +1,15 @@
+// link : https://codeforces.com/group/LKPSX7Pt2W/contest/323155/problem/J
+// author : @Mohamed Ibrahim
+
+#include<bits/stdc++.h> 
+using namespace std;   
+  
+ 
+int main()
+{
+	string s;
+	cin>>s;
+	int l = s.length();
+	cout<<(26*(l+1)-l)<<endl;
+	return 0;
+}
