4c9e79b655f3c694f747671c66cf57e69be453e2
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-05-30 23:17:52 +0200

    Create Domino piling.cpp

---
+++ Math - Geometry/Domino piling.cpp
@@ -0,0 +1,13 @@
+//link : https://codeforces.com/problemset/problem/50/A
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int M, N;
+    cin >> M >> N;
+    cout << M * N / 2 << endl;
+    return 0;
+}
