90b2300081ff38351f4843a24757fbd17ead7e03
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-05-30 23:31:29 +0200

    Create E. Maximum Distinct Numbers.cpp

---
+++ Math - Geometry/E. Maximum Distinct Numbers.cpp
@@ -0,0 +1,15 @@
+//link: https://codeforces.com/group/MWSDmqGsZm/contest/223338/problem/E
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	long long num;
+	cin>>num;
+	for(long long i=1;;i++){
+		if(i > num){
+			cout<<i-1;
+			return 0;
+		} else {
+			num -= i;
+		}
+	}
+}
