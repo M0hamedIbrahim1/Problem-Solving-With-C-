b0c82a4e13c3d8efe5ddddbddb21ca3172055459
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-06-01 22:23:36 +0200

    Create Picky.cpp

---
+++ Math - Geometry/Picky.cpp
@@ -0,0 +1,20 @@
+//link : https://codeforces.com/group/x1IbeOVM4y/contest/287840/problem/B
+//author : @Mohamed Ibrahim
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+ int x,y,r;
+ cin>>x>>y;
+
+ r=x/y;
+
+if(r>=1)
+ cout<<"1"<<endl;
+else
+cout<<"0"<<endl;
+
+}
