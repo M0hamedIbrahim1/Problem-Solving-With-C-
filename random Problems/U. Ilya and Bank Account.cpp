b1fe5758643dbb5ce7ad4e3dd4d2cb6c9a61c395
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-06-02 23:33:15 +0200

    Create U. Ilya and Bank Account.cpp

---
+++ random Problems/U. Ilya and Bank Account.cpp
@@ -0,0 +1,23 @@
+//link : https://codeforces.com/group/MWSDmqGsZm/contest/223206/problem/U
+//author : @Mohamed Ibrahim
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    if (n >= 0)
+    {
+        cout << n << endl;
+    }
+    else
+    {
+        int a = n / 10;
+        int b = n / 100 * 10 + n % 10;
+        cout << max(a, b) << endl;
+    }
+    return 0;
+}
