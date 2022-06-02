08b5350a1e876c95ee7ca43151dbc67453f69fca
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-06-02 23:30:57 +0200

    Create 50A - Domino piling.cpp

---
+++ random Problems/50A - Domino piling.cpp
@@ -0,0 +1,9 @@
+//link : https://codeforces.com/problemset/problem/50/A
+//author : @Mohamed Ibrahim
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int x,y; cin>>x>>y;
+    cout<<x*y/2<<endl;
+}
