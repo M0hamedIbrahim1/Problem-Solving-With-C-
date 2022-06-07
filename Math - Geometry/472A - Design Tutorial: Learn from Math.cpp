90c48f3fbd6e0a7e711936404bd9f3254cef24fc
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-06-07 12:37:43 +0200

    Create 472A - Design Tutorial: Learn from Math.cpp

---
+++ Math - Geometry/472A - Design Tutorial: Learn from Math.cpp
@@ -0,0 +1,16 @@
+// link :  https://codeforces.com/problemset/problem/472/A
+// author : @Mohamed Ibrahim
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	long long n;
+	cin>>n;
+	if(n%2==0){
+		cout<<n-4<<" "<<4;
+	}else{
+		cout<<n-9<<" "<<9;
+	}
+}
+	
+
