1c804154c8849a6ff0ede6dfb45e1148b87b5fe4
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-06-09 15:14:58 +0200

    Create Even Array.cpp

---
+++ random Problems/Even Array.cpp
@@ -0,0 +1,25 @@
+//link:   https://codeforces.com/problemset/problem/1367/B
+//author: Mohamed Ibrahim
+
+#include <bits/stdc++.h>
+using namespace std;
+int main (){
+	int q;
+	cin>>q;
+	while (q--)
+	{	
+		even=odd=0;
+        int n;
+        cin >> n;
+        for(int i = 0 ; i<n ; ++i){
+        	cin >> arr[i];
+		}   
+        for(int i = 0 ; i<n ; ++i){
+            if(arr[i]%2==0 && i%2!=0) even++;
+            else if(arr[i]%2!=0 && i%2==0) odd++;
+        }
+
+        if(even==odd) cout << even <<endl;
+        else cout << -1 <<endl;	
+	}
+}
