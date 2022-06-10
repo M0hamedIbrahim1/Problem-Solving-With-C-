57111f6bb0beef407a474644e12749d3b9a4ebff
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-06-10 21:19:06 +0200

    Create  Maximum GCD.cpp

---
+++ Math - Geometry/Maximum GCD.cpp
@@ -0,0 +1,14 @@
+//link:   https://codeforces.com/contest/1370/problem/A
+//author: Mohamed Ibrahim
+	
+#include <bits/stdc++.h>
+using namespace std;
+int main (){
+	int t;
+	cin>>t;
+	while(t--){
+		long long n;
+		cin>>n;
+		cout<<n/2<<endl;
+	}
+}
