28d8ea9de5dc5798596e6efaeb3d28210fd091ae
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-06-11 08:30:37 +0200

    Create Again Twenty Five!.cpp

---
+++ Math - Geometry/Again Twenty Five!.cpp
@@ -0,0 +1,12 @@
+//link   : https://codeforces.com/problemset/problem/630/A
+//author : @Mohamed Ibrahim
+
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+//----------  last two digit of  5^n  is 25 for all possibilities :) 
+	cout<<"25";
+}
+ 
