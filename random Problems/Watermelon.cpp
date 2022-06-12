8077321601bddcd7a7ba46d5857d21c732be5ff5
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-06-12 22:40:18 +0200

    Create Watermelon.cpp

---
+++ random Problems/Watermelon.cpp
@@ -0,0 +1,19 @@
+//link:   https://codeforces.com/group/MWSDmqGsZm/contest/223206/problem/B
+//author: Mohamed Ibrahim
+	
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+int weight;
+cin>>weight;
+if(weight==2){
+	cout<<"NO";
+	return 0;
+}
+if(weight%2==0){
+	cout<<"YES";
+}
+else{
+	cout<<"NO";
+}
+}
