483a4dd4edf6e1c5cf6794609500de526a2df56e
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-06-13 22:05:13 +0200

    Create 1283A - Minutes Before the New Year.cpp

---
+++ random Problems/1283A - Minutes Before the New Year.cpp
@@ -0,0 +1,14 @@
+//link:   https://codeforces.com/problemset/problem/1283/A
+//author: Mohamed Ibrahim
+	
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		int h,m;
+		cin>>h>>m;
+		cout<<(24-h) * 60 - m<<endl;
+	}
+}
