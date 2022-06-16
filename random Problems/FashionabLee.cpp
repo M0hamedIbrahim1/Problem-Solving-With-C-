85cb5e9576332934d777208083826c360363a537
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-06-16 19:48:18 +0200

    Create FashionabLee.cpp

---
+++ random Problems/FashionabLee.cpp
@@ -0,0 +1,18 @@
+//link   : https://codeforces.com/problemset/problem/1369/A
+//author : @Mohamed Ibrahim
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--){
+        int n; cin>>n;
+        if(n % 4 == 0){
+                cout<<"YES\n";
+        }else{
+                cout<<"NO\n";
+        }
+	}
+}
