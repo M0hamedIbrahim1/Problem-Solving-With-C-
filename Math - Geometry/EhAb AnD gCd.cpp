ef4a46ca1295e759e2e14a7a8ca2f311f32a042c
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-06-17 11:08:15 +0200

    Create  EhAb AnD gCd.cpp

---
+++ Math - Geometry/EhAb AnD gCd.cpp
@@ -0,0 +1,15 @@
+//link   : https://codeforces.com/problemset/problem/1325/A
+//author : @Mohamed Ibrahim
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int t;
+	cin>>t;
+	long long x,res;
+	while(t--){
+		cin>>x;
+		cout<<"1 "<<x-1<<endl;
+	}
+
+}
