d17ccb2a951a73c7b67eaff674bfdb2b64ddbfc2
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-06-17 10:24:25 +0200

    Create  Mahmoud and Ehab and the even-odd game.cpp

---
+++ Math - Geometry/Mahmoud and Ehab and the even-odd game.cpp
@@ -0,0 +1,12 @@
+//link   : https://codeforces.com/problemset/problem/959/A
+//author : @Mohamed Ibrahim
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+	long long n;
+	cin>>n;
+	if(n % 2) cout<<"Ehab";
+	else cout<<"Mahmoud";
+}
