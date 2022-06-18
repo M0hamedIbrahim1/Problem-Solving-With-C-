702bc34cab9ef0cc675e56816c353f7d3b76ed7b
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-06-18 08:09:23 +0200

    Create CopyCopyCopy.cpp

---
+++ Arrays/CopyCopyCopy.cpp
@@ -0,0 +1,28 @@
+//link   : https://codeforces.com/problemset/problem/1325/B
+//author : @Mohamed Ibrahim
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	
+	int t, n, arr[100005];
+	cin >> t;
+	while (t--) {
+		cin >> n;
+
+		for (int i = 0; i < n; i++) {
+			cin >> arr[i];
+		}
+
+		sort(arr, arr + n);
+		int len = unique(arr, arr + n) - arr;
+
+		cout << len << endl;
+	}
+
+	return 0;
+}
