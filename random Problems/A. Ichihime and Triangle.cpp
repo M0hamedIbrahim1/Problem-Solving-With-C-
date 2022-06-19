5ae16a0361d0a72cb8f8042eefdba2b72e135b9f
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-06-19 21:18:59 +0200

    Create A. Ichihime and Triangle.cpp

---
+++ random Problems/A. Ichihime and Triangle.cpp
@@ -0,0 +1,13 @@
+//link:   https://codeforces.com/problemset/problem/1337/A
+//author: Mohamed Ibrahim
+	
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int t,a,b,c,d;
+	cin>>t;
+	while(t--){
+		cin>>a>>b>>c>>d;
+		cout<<a<<" "<<c<<" "<<c<<endl;
+	}
+}
