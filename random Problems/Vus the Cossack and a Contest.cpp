1c98f518e609e4099c14464486a0a602c41787d9
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-06-20 18:59:54 +0200

    Create Vus the Cossack and a Contest.cpp

---
+++ random Problems/Vus the Cossack and a Contest.cpp
@@ -0,0 +1,14 @@
+//link:   https://codeforces.com/problemset/problem/1186/A
+//author: Mohamed Ibrahim
+	
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int a,b,c;
+	cin>>a>>b>>c;
+	if(b>=a && c >=a){
+		cout<<"YES";
+	}else{
+		cout<<"NO";
+	}
+}
