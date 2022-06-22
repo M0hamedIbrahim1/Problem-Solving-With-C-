1cd55c8c8c11d99401739389c5cd703e90eda51e
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-06-22 20:32:47 +0200

    Update Announcement .cpp

--- random Problems/Announcement .cpp
+++ random Problems/Announcement .cpp
@@ -5,19 +5,19 @@
 using namespace std;
 int main()
 {
-int freq[100000],count=0;
 long long size;
+int count = 0;
 cin>>size;
 long long arr[size];
 for(int i =0;i<size;i++)
 {
-	cin>>arr[i];
-	freq[arr[i]]++;
+	cin>>arr[i];	
 }
-for(int i =0;i<size;i++){
-	if(freq[arr[i]]>1){
-		count+=freq[arr[i]]-1;
-		freq[arr[i]]=0;
+sort(arr,arr+size);
+
+for(int i =0;i<=size-1;i++){
+	if(arr[i]==arr[i+1]){
+		count++;
 	}
 }
 if(count==0){
