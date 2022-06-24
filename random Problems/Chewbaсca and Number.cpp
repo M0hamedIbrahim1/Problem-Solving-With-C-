01d4fbaa0c554cdfd36f79448b58b0a465d469cc
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-06-24 22:49:11 +0200

    Create Chewbaсca and Number.cpp

---
+++ random Problems/Chewbaсca and Number.cpp
@@ -0,0 +1,18 @@
+//link:   https://codeforces.com/group/MWSDmqGsZm/contest/223340/problem/H
+//author: Mohamed Ibrahim
+
+#include<bits/stdc++.h>
+using namespace std;						  
+int main() {
+	string s;
+	cin>>s;
+	for(int i =0;i<s.length();i++){
+		if(s[i]=='9'&&i==0){
+			cout<<'9';
+		}else if(s[i]-'0'>4){
+			cout<< 9 - (s[i]-'0');	
+		}else{
+			cout<<s[i];
+		}
+	}
+}
