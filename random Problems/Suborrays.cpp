0a1d4740da4a2262c8fba15db7c3d3021105ec44
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-06-24 18:59:00 +0200

    Create Suborrays.cpp

---
+++ random Problems/Suborrays.cpp
@@ -0,0 +1,20 @@
+//link:   https://codeforces.com/problemset/problem/1391/A
+//author: Mohamed Ibrahim
+	
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int num;
+		cin>>num;
+		while(num){
+			cout<<num<<" ";
+			num--;
+		}
+		cout<<endl;
+	}
+}
