e0afb622673939864e3f5962cb97877733b85406
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-06-27 23:31:27 +0200

    Create C. XOR Problem.cpp

---
+++ random Problems/C. XOR Problem.cpp
@@ -0,0 +1,18 @@
+//link:   https://codeforces.com/group/yw6xPXn7ZO/contest/375911/problem/C
+//author: Mohamed Ibrahim
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+ long long n1,n2;
+ cin>>n1>>n2;
+ cout<<n1-n2;
+}
+
+
+
+	
+
+
+	
