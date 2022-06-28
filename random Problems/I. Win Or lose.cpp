6c50bc29b8471dcbfc3e8939789e67740902e916
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-06-28 23:34:15 +0200

    Create I. Win Or lose.cpp

---
+++ random Problems/I. Win Or lose.cpp
@@ -0,0 +1,21 @@
+//link:   https://codeforces.com/group/yw6xPXn7ZO/contest/375911/problem/I
+//author: Mohamed Ibrahim
+
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+
+ long long n;
+ cin>>n;
+ 
+if(n%4==0){
+ 	cout<<"Draw";
+ }else{
+ 	cout<<"Amr";
+ }
+	
+
+}
+ 
