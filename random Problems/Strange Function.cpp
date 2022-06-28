7b7ed5f7129d6aa562dccadc0f51795603395644
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-06-28 22:04:54 +0200

    Create Strange Function.cpp

---
+++ random Problems/Strange Function.cpp
@@ -0,0 +1,16 @@
+//link:   https://codeforces.com/problemset/problem/1455/A
+//author: Mohamed Ibrahim
+
+#include<bits/stdc++.h>
+using namespace std;						  
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		string s;
+	    cin>>s;
+	    cout<<s.size()<<'\n';
+	}
+}
