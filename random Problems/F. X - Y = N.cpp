34db456b5e06759b4c9489575fd9120eaac189d1
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-06-28 23:31:41 +0200

    Create F. X - Y = N.cpp

---
+++ random Problems/F. X - Y = N.cpp
@@ -0,0 +1,15 @@
+//link:   https://codeforces.com/group/yw6xPXn7ZO/contest/375911/problem/F
+//author: Mohamed Ibrahim
+
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+ long long n1,n2,n3;
+ cin>>n1;
+ 
+ cout<<n1+1<<" "<<"1";
+ 
+ 
+  
+}
