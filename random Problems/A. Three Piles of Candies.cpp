92f2049c540845c1a7443b12d5b607fb2befee5b
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-06-30 19:19:00 +0200

    Create A. Three Piles of Candies.cpp

---
+++ random Problems/A. Three Piles of Candies.cpp
@@ -0,0 +1,18 @@
+//link:   https://codeforces.com/problemset/problem/1196/A
+//author: Mohamed Ibrahim
+
+#include<bits/stdc++.h>
+using namespace std;						  
+int main()
+{
+	long long t,n,a,b,c;
+	cin>>t;
+    while(t--)
+    {
+		cin>>a>>b>>c;
+		cout<<(a+b+c) / 2;
+
+		cout<<endl;
+    }
+
+}
