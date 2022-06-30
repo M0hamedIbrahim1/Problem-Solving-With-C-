8dece9caedd22362efb98cd46b152c0909ed5d83
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-06-30 15:54:06 +0200

    Create Digits sum.cpp

---
+++ random Problems/Digits sum.cpp
@@ -0,0 +1,16 @@
+//link:   https://codeforces.com/problemset/problem/1553/A
+//author: Mohamed Ibrahim
+
+#include<bits/stdc++.h>
+using namespace std;						  
+int main()
+{
+	int t,n;
+	cin>>t;
+    while(t--)
+    {
+        cin>>n;
+        cout<<(n+1)/10<<'\n';
+    }
+
+}
