dbb512e9dbf2d55e95b03dbda05a43f0888fb4fd
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-07-01 17:55:43 +0200

    Create  Mezo Playing Zoma.cpp

---
+++ random Problems/Mezo Playing Zoma.cpp
@@ -0,0 +1,14 @@
+//link:   https://codeforces.com/problemset/problem/1285/A
+//author: Mohamed Ibrahim
+
+#include<bits/stdc++.h>
+using namespace std;						  
+int main()
+{
+	int size;
+	cin>>size;
+	string s;
+	cin>>s;
+	cout<<size+1;
+
+}
